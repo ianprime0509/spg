582a74447184fbef44a7337289efe1a1d9e22bb9
Ian Johnson <ianprime0509@gmail.com>
2019-03-03 17:41:10 -0500

    Add supporting documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# spg - simple pager
+
+Spg is a simple terminal pager, designed to be as small as possible. By
+default, it mimics the keybindings of less(1), but this can be configured
+by modifying your personal copy of `config.h`. More details about spg
+can be found in the enclosed instruction book (`spg.1`).
+
--- config.def.h
+++ config.def.h
@@ -1,3 +1,19 @@
+/*
+ * Keybindings are defined in the following format:
+ * { key, function, argument }
+ * where argument is a union whose members are named after their types'
+ * printf(3) format strings. For a function that doesn't take an argument, any
+ * value for the argument will do ({ 0 } expresses this well).
+ *
+ * For reference, here is a list of the functions provided:
+ * pagedown(lf) - scroll down by lf screens
+ * pageup(lf) - scroll up by lf screens
+ * scrolldown(zu) - scroll down by zu lines
+ * scrollup(zu) - scroll up by zu lines
+ * scrolltop() - scroll to the top of the document
+ * scrollbot() - scroll to the bottom of the document
+ * quit() - exit spg
+ */
 static Key keys[] = {
 	{ 'j', scrolldown, { .zu = 1 } },
 	{ 'k', scrollup, { .zu = 1 } },
